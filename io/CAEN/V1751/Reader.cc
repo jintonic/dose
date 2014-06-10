306d9b133012a76cfe1a19b0b47b316ed051718e
Jing Liu <jing.leon@gmail.com>
2014-06-10 23:24:15 +0900

    cleaned up

--- io/CAEN/V1751/Reader.cc
+++ io/CAEN/V1751/Reader.cc
@@ -1,11 +1,10 @@
 #include <cmath>
 using namespace std;
 
-#include <Units.h>
-using namespace UNIC;
+#include <WF.h>
+using namespace NICE;
 
 #include "Reader.h"
-using namespace NICE;
 
 Reader::Reader(int run, int sub, const char *dir) :
 WFs(run), Logger(), fRaw(0)
@@ -153,75 +152,69 @@ void Reader::ReadRunInfo(int i)
 
 void Reader::ReadEvent(int i)
 {
-   int evt_size=fSize[i]-sizeof(CAEN_DGTZ_LHEADER_t);
+   // read data
+   size_t evt_size=fSize[i]-sizeof(CAEN_DGTZ_LHEADER_t);
    char *content = new char[evt_size];
    fRaw->read(content, evt_size);
 
-   char *ptr = content;
-   int processed=0, channel=0;
-   while(processed<evt_size) {
-      if (channel>=nch) {
-         Warning("ReadEvent", "ch. %d >= %d (max), skipped", channel, nch);
-         continue;
-      }
-      WF* wf = (WF*) wfs.At(channel);
-      wf->samples.resize(0);
+   // decode data
+   UInt_t *data = (UInt_t*) (content);
+   unsigned int processed=0;
+   unsigned short channel=0;
+   while(processed<evt_size && channel<nch) {
+      processed+=data[0]*4; // data size in byte
 
-      // decode waveform data
-      unsigned int *data = (unsigned int*) (ptr);
+      WF* wf = At(channel); // must have been initialized
+      wf->samples.resize(0); // reset samples
+      wf->ctrg = data[1]; // trigger time for this channel
 
-      int data_size = data[0]*4; // data size in byte
-      ptr+=data_size; // move pointer to next raw waveform
-      processed+=data_size;
+      // ped_type==0 -> integer baseline, ==1 -> float baseline
+      int ped_type = (data[2]>>31)&0x1;
+      // 2^(ped_N_samples+2)= number of samples used for baseline calculation
+      int ped_N_samples = (data[2]>>28)&0x7; 
+      int ped_sum = data[2]&0x7fff; // sum of ADC counts
+      if (ped_type==0) wf->ped = data[2]; // integer
+      else wf->ped = ped_sum/pow(2.,ped_N_samples+2);
 
-      wf->ttrg = data[1]*8*ns; // trigger time for this channel
-
-      // baselineFlag==0 -> integer baseline, ==1 -> float baseline
-      int baselineFlag = (data[2]>>31)&0x1;
-      // 2^(baselineNsam+2)= number of samples used for baseline calculation
-      int baselineNsam = (data[2]>>28)&0x7; 
-      int baselineSum = data[2]&0x7ffff; // sum of ADC counts
-      if (baselineFlag==0) wf->ped = data[2]; // integer
-      else wf->ped = baselineSum/pow(2.,baselineNsam+2);
-
-      Bool_t isBaseline=kTRUE;
+      bool isBaseline=true;
       size_t nSamples=0; // total number of samples
       size_t nSuppressed=0; // number of suppressed samples
-      for (int i=3;i<data_size/4;i++) {
-         int nZippedSamples = (data[i]>>30);
+      size_t nPulses=0; // number of not suppressed regions
+      for (unsigned int i=3;i<data[0];i++) {
+         unsigned int nZippedSamples = (data[i]>>30);
          if (nZippedSamples==3) { // got 3 zipped samples
             // decode 3 samples
-            wf->samples.push_back((data[i])     & 0x000003FF);
-            wf->samples.push_back((data[i]>>10) & 0x000003FF);
-            wf->samples.push_back((data[i]>>20) & 0x000003FF);
-
-            if (isBaseline==kTRUE) {
+            wf->samples.push_back((data[i])     & 0x3FF);
+            wf->samples.push_back((data[i]>>10) & 0x3FF);
+            wf->samples.push_back((data[i]>>20) & 0x3FF);
+            // add a pulse
+            if (isBaseline==true) {
                Pulse pls;
                pls.bgn=nSamples;
                pls.end=nSamples;
                wf->pulses.push_back(pls);
+               nPulses++;
             }
-            isBaseline=kFALSE;
-
+            isBaseline=false;
             nSamples += 3;
          } else if (nZippedSamples==0) { // no zipped samples
-            if (isBaseline==kFALSE)
-               wf->pulses.back().end=nSamples;
-            isBaseline=kTRUE;
-
             // get number of skipped samples
-            int nSkippedSamples = data[i]*8;
-            if (nSkippedSamples+nSamples>=12000) {
+            unsigned int nSkippedSamples = data[i]*8;
+            if (nSkippedSamples+nSamples>nmax) {
                Warning("ReadEvent",
-                     "number of skipped samples >=12000, break!");
+                     "%d (skipped) + %zu (real) > %d, break!",
+                     nSkippedSamples, nSamples, nmax);
                break;
             }
             // fill skipped samples with baseline
             nSuppressed+=nSkippedSamples;
-            for (int j=0; j<nSkippedSamples; j++) {
+            nSamples+=nSkippedSamples;
+            for (unsigned int j=0; j<nSkippedSamples; j++)
                wf->samples.push_back(wf->ped);
-               nSamples++;
-            }
+            // finish adding a pulse
+            if (isBaseline==kFALSE) 
+               wf->pulses.back().end=nSamples;
+            isBaseline=true;
          } else { // skip the rest samples if nZippedSamples!=0 or 3
             Warning("ReadEvent",
                   "number of zipped samples in PMT %d = %d, skip the rest.",
@@ -229,81 +222,73 @@ void Reader::ReadEvent(int i)
             break;
          }
       }
-      // if all samples are suppressed, skip this wf
-      if (nSuppressed==nSamples) {
-         channel++;
-         continue;
-      }
       // check length of wf
       if (nSamples!=nmax) {
          Warning("ReadEvent",
-               "number of samples in PMT %2d (ch.%d) is %zu,",
-               wf->pmt.id, channel, nSamples);
+               "number of samples in PMT %2d is %zu,", wf->pmt.id, nSamples);
          Warning("ReadEvent",
                "while total number of samples is %d", nmax);
       }
-      // update end of last pulse
-      if (wf->pulses.back().end==wf->pulses.back().bgn)
-         wf->pulses.back().end=nSamples;
+      // update end of last pulse if the last sample is not 0-suppressed
+      data+=data[0]; // move pointer to next raw waveform
+      channel++; // increase channel count
 
       // re-calculate baseline for runs with integer hardware baseline
-      if (baselineFlag==0) { // integer baseline
-         // baseline calculation
-         double realBaseline=0.; double realBaselineSquare=0.;
-         size_t nBaselineSamples_max=96, nBaselineSamples = 0;
-         if(nSamples<nBaselineSamples_max) nBaselineSamples_max = nSamples;
-         for (int i=wf->pulses[0].bgn; i<int(wf->pulses[0].bgn+nBaselineSamples_max); i++) {
-            realBaseline += wf->samples[i];
-            realBaselineSquare += wf->samples[i]*wf->samples[i];
-            nBaselineSamples++;
-         }
-         realBaseline = realBaseline/nBaselineSamples;
-         realBaselineSquare = realBaselineSquare/nBaselineSamples;
-         wf->prms = sqrt(realBaselineSquare - realBaseline*realBaseline);
+      if (nPulses==0) continue; // no unsuppressed region
+      if (wf->pulses.back().end==wf->pulses.back().bgn)
+         wf->pulses.back().end=nSamples;
+      if (nSuppressed==nSamples) continue; // all samples are suppressed
+      if (ped_type==1) continue; // already float baseline
+      if (nfw<30) {
+         Warning("ReadEvent", "no enough samples to calculate baseline");
+         continue;
+      }
+      short nBaselineSamples = 30;
 
-         double realBaseline_tmp = realBaseline;
-         nBaselineSamples = 0;
-         realBaseline = 0.; realBaselineSquare = 0.;
-         int baseline_calc_flag = 0;
-         double sample_prev = 0; double sampleSquare_prev = 0;
-         for (int i=wf->pulses[0].bgn; i<int(wf->pulses[0].bgn+nBaselineSamples_max); i++) {
-            if(baseline_calc_flag==1){
-               if((realBaseline_tmp-wf->samples[i])<2){
-                  realBaseline += wf->samples[i];
-                  realBaselineSquare += wf->samples[i]*wf->samples[i];
-                  nBaselineSamples++;
-                  baseline_calc_flag = 0;
-               }else{
-                  realBaseline  = realBaseline - sample_prev;
-                  realBaselineSquare  = realBaselineSquare - sampleSquare_prev;
-                  nBaselineSamples = nBaselineSamples - 1;
-                  baseline_calc_flag = 2;
-               }
-            }else if(baseline_calc_flag==2){
-               if((realBaseline_tmp-wf->samples[i])<2){
-                  realBaseline += wf->samples[i];
-                  realBaselineSquare += wf->samples[i]*wf->samples[i];
-                  nBaselineSamples++;
-                  baseline_calc_flag = 0;
-               }
-            }else{
-               realBaseline += wf->samples[i];
-               realBaselineSquare += wf->samples[i]*wf->samples[i];
+      double baseline=0;
+      for (int i=wf->pulses[0].bgn; i<wf->pulses[0].bgn+nBaselineSamples; i++)
+         baseline += wf->samples[i]/nBaselineSamples;
+
+      double tmpBaseline = baseline, baseline2=0;
+      nBaselineSamples = 0;
+      baseline = 0.;
+      int baseline_calc_flag = 0;
+      double sample_prev = 0; double sample2_prev = 0;
+      for (int i=wf->pulses[0].bgn; i<wf->pulses[0].bgn+nfw; i++) {
+         if(baseline_calc_flag==1){
+            if((tmpBaseline-wf->samples[i])<2){
+               baseline += wf->samples[i];
+               baseline2 += wf->samples[i]*wf->samples[i];
                nBaselineSamples++;
-               if((realBaseline_tmp-wf->samples[i])>2){
-                  baseline_calc_flag=1;
-                  sample_prev = wf->samples[i];
-                  sampleSquare_prev = wf->samples[i]*wf->samples[i];
-               }
+               baseline_calc_flag = 0;
+            }else{
+               baseline  = baseline - sample_prev;
+               baseline2  = baseline2 - sample2_prev;
+               nBaselineSamples = nBaselineSamples - 1;
+               baseline_calc_flag = 2;
+            }
+         }else if(baseline_calc_flag==2){
+            if((tmpBaseline-wf->samples[i])<2){
+               baseline += wf->samples[i];
+               baseline2 += wf->samples[i]*wf->samples[i];
+               nBaselineSamples++;
+               baseline_calc_flag = 0;
+            }
+         }else{
+            baseline += wf->samples[i];
+            baseline2 += wf->samples[i]*wf->samples[i];
+            nBaselineSamples++;
+            if(abs(tmpBaseline-wf->samples[i])>2){
+               baseline_calc_flag=1;
+               sample_prev = wf->samples[i];
+               sample2_prev = wf->samples[i]*wf->samples[i];
             }
          }
-         realBaseline = realBaseline/nBaselineSamples;
-         realBaselineSquare = realBaselineSquare/nBaselineSamples;
-         wf->prms = sqrt(realBaselineSquare - realBaseline*realBaseline);
-
-         wf->ped = realBaseline;
       }
-      channel++;
+      baseline = baseline/nBaselineSamples;
+      baseline2 = baseline2/nBaselineSamples;
+      wf->prms = sqrt(baseline2 - baseline*baseline);
+      wf->ped = baseline;
    }
 
    delete[] content;
