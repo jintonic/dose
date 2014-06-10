53561de76b0f99a4babc807804d9d6b1f219c3ff
Jing Liu <jing.leon@gmail.com>
2014-06-10 17:31:33 +0900

    It uses TClonesArray instead of inheriting from it

--- core/WFs.h
+++ core/WFs.h
@@ -3,21 +3,32 @@
 
 #include <TClonesArray.h>
 
-#include "WF.h"
-
-namespace NICE { class WFs; }
-
-class NICE::WFs : public TClonesArray
+namespace NICE {
+   class WF;
+   class WFs;
+}
+/**
+ * Information of waveform array
+ * It contains some global information common for all channels.
+ *
+ * It uses TClonesArray instead of inheriting from it because TClonesArray is
+ * created merely as a container.
+ *
+ * It loads PMT database to initialize the TClonesArray of empty waveforms with
+ * PMT information. Loading database is done here instead of in the NICE::PMT
+ * class to avoid passing run, file name and location to NICE::PMT.
+ */
+class NICE::WFs : public TObject
 {
    private:
-      PMT* Map(int ch); ///< map ch to pmt
-      void LoadTimeOffset(PMT* aPMT);
-      void LoadMeanOf1PE(PMT* aPMT);
-      void LoadStatus(PMT* aPMT);
+      WF* Map(int ch); ///< map ch to WF::pmt
+      void LoadTimeOffset(WF* wf); ///< load dt offset to WF::pmt
+      void LoadMeanOf1PE(WF* wf); ///< load 1PE mean to a WF::pmt
+      void LoadStatus(WF* wf); ///< load status to a WF::pmt
 
    protected:
       /**
-       * /path/to/pmt
+       * Path to PMT database
        */
       TString fDB; //! /path/to/pmt/
 
@@ -37,38 +48,44 @@ class NICE::WFs : public TClonesArray
       /**
        * Trigger count
        */
-      int cnt; // trigger count
+      unsigned int cnt; // trigger count
       /**
        * Seconds to Unix epoch
+       * Valid till year 2038
        */
-      int sec; // Linux time of event in second
+      unsigned int sec; // Linux time of event in second
       /**
        * Total number of available channels
+       * Filled after loading PMT database
        */
-      short nch; // number of available channels
+      unsigned short nch; // number of available channels
       /**
        * Maximal number of samples of a waveform
+       * Filled after loading run header
        */
-      size_t nmax; // max number of samples of a wf
+      unsigned short nmax; // max number of samples of a wf
       /**
        * Number of samples after non-0-suppressed window
+       * Filled after loading run header
        */
-      int nfw; // # of samples after non-0-suppressed window
+      unsigned short nfw; // # of samples after non-0-suppressed window
       /**
        * Number of samples before non-0-suppressed window
+       * Filled after loading run header
        */
-      int nbw; // # of samples before non-0-suppressed window
+      unsigned short nbw; // # of samples before non-0-suppressed window
       /**
        * Threshold for software 0-suppression
+       * unit: ADC count
        */
-      int thr; // threshold for software 0-suppression
+      unsigned short thr; // threshold for software 0-suppression
+      /**
+       * Waveform array
+       */
+      TClonesArray wfs; // waveform array
 
    public:
-      WFs(int run=999999) :
-         TClonesArray("NICE::WF", 8),
-         run(run), sub(-1), evt(-1), cnt(-1), sec(-1), nmax(0), 
-         nfw(0), nbw(0), thr(0) { nch=fSize; }
-
+      WFs(int run=999999);
       virtual ~WFs() {};
 
       void Initialize(const char *db="/path/to/pmt");
