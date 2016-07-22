6abeb85d9ca29e81e571e28fccca49ba908b3c53
Jing LIU <jing.liu@usd.edu>
2016-07-22 13:19:48 -0500

    set software threshold

--- io/CAEN/V1751/b2r.C
+++ io/CAEN/V1751/b2r.C
@@ -26,6 +26,7 @@ void b2r(int run, int sub=1, const char* dir=".")
    cout<<input.GetEntries()<<" entries to be processed"<<endl;
    for (int i=0; i<input.GetEntries(); i++) {
       input.GetEntry(i);
+      if (input.thr==0) input.thr=3; // set threshold if no hardware one
       for (int ch=0; ch<input.nch; ch++) input.Scan(ch);
       t.Fill();
       if (i%5000==0) cout<<"entry "<<i<<" processed"<<endl;
