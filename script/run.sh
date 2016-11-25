1402fb33c57569e4d2689d96a1a911da196d39bd
Jing LIU <jing.liu@usd.edu>
2016-11-25 02:04:15 -0600

    formated output

--- script/run.sh
+++ script/run.sh
@@ -7,24 +7,26 @@ BEGIN { isSetting=1; isHeader=0; FS="="; }
     isSetting=0; 
     isHeader=1;
   }
-  if (isSetting) {
-    next;
-  }
+  if (isSetting) next;
+  if (isHeader) printf("%s ", substr($2,2));
 
-  if (isHeader) {
-    printf("%s ", substr($2,2));
-  }
   if ($2 == " \" comment\"") {
     FS=" ";
     isHeader=0;
   } 
 
   column=substr($2,1,1);
-  if (column == "A") {
-    printf("\n");
-  }
-  for (i=4; i<=NF; i++) {
-    printf("%s ", $i);
-  }
+  if (column == "A") printf("\n %3d ", $4);
+  else if (column == "D") printf("%9s ", $4);
+  else if (column == "E") printf("%6s ", $4);
+  else if (column == "F") printf("%9s ", $4);
+  else if (column == "G") printf("%6.1f ", $4);
+  else if (column == "H") printf("%5s ", $4);
+  else if (column == "I") printf("%11s ", $4);
+  else if (column == "J") printf("%4d ", $4);
+  else if (column == "K") printf("%11s ", $4);
+  else if (column == "L") printf("%3d ", $4);
+  else for (i=4; i<=NF; i++) printf("%s ", $i);
 }
 ' $NICESYS/script/run.sc > $NICESYS/script/run.txt
+sed -i 's/"//g' $NICESYS/script/run.txt
