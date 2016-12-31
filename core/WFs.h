5e7490b9d2f16e19a8acb67608efa2e7ed0d48f2
Jing LIU <jing.liu@usd.edu>
2016-12-31 03:05:43 -0600

    added t

--- core/WFs.h
+++ core/WFs.h
@@ -33,60 +33,46 @@ class NICE::WFs : public TObject
       TString fDB; //! /path/to/pmt/
 
    public:
-      /**
-       * Run number
-       */
-      int run; // run number
-      /**
-       * Sub run number
-       */
-      short sub; // sub run number
-      /**
-       * Event id
-       */
-      int evt; // event id
-      /**
-       * Trigger count
-       */
-      unsigned int cnt; // trigger count
+      int run; ///< run number
+      short sub; ///< sub run number
+      int id; ///< event id (number of triggered events)
+      int cnt; ///< trigger count
+      double t; ///< nano sec calculated from trigger count
       /**
        * Seconds to Unix epoch
-       * Valid till year 2038
+       * Valid till year 2038. = sec + usec*1e-6 
        */
-      unsigned int sec; // Linux time of event in second
+      double sec;
       /**
        * Total number of available channels
        * Filled after loading PMT database
        */
-      unsigned short nch; // number of available channels
+      unsigned short nch;
       /**
        * Maximal number of samples of a waveform
        * Filled after loading run header
        */
-      unsigned short nmax; // max number of samples of a wf
+      unsigned short nmax;
       /**
        * Number of samples after non-0-suppressed window
        * Filled after loading run header
        */
-      unsigned short nfw; // # of samples after non-0-suppressed window
+      unsigned short nfw;
       /**
        * Number of samples before non-0-suppressed window
        * Filled after loading run header
        */
-      unsigned short nbw; // # of samples before non-0-suppressed window
+      unsigned short nbw;
       /**
        * Threshold for software 0-suppression
        * unit: ADC count
        */
-      unsigned short thr; // threshold for software 0-suppression
-      /**
-       * Waveform array
-       */
-      TClonesArray wf; // waveform array
+      unsigned short thr;
+      TClonesArray wf; ///< waveform array
 
    public:
-      WFs(int run=999999) : TObject(), run(run), sub(-1), evt(-1), cnt(0), sec(0),
-      nch(8), nmax(0), nfw(0), nbw(0), thr(0) {};
+      WFs(int run=999999) : TObject(), run(run), sub(-1), id(-1), cnt(-1),
+      t(-1), sec(-1), nch(8), nmax(0), nfw(0), nbw(0), thr(0) {};
       virtual ~WFs() {};
 
       void Initialize(const char *db="/path/to/pmt");
@@ -94,7 +80,7 @@ class NICE::WFs : public TObject
       WF* At(unsigned short i) const;
       WF* operator[](unsigned short i) const { return At(i); }
 
-      ClassDef(WFs,1);
+      ClassDef(WFs,2);
 };
 
 #endif
