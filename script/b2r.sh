53bd8add5a53bdd623b40f17b06a08911ae2d86c
Jing LIU <jing.liu@usd.edu>
2017-01-06 15:46:51 -0600

    specified max run as well

--- script/b2r.sh
+++ script/b2r.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# convert all binary files to root files
+# convert a range of binary files to root files
 
 # figure out valid range of runs, [min, max]
 dir0=`ls -1 $NICEDAT | egrep '^[0-9]+$' | head -1`
@@ -13,24 +13,71 @@ max=`echo ${max:0:6} | bc`
 echo "Run ranges from $min to $max"
 
 # skip old runs unless specified otherwise
-min=171
-read -p "Please set minimal run number (<Enter> to accept default: $min): " run
-if [ X$run = X ]; then run=$min; fi
-read -p "Overwrite existing root files? (y/N): " overwrite
+old=`ls -1 $NICEDAT/$dir1/run_??????.??????.root | tail -1`
+old=${old#*run_}
+old=`echo ${old:0:6} | bc`
+echo "Please set minimal run number (<Enter> to accept default: $old):"
+read -e -n ${#max} input
+if [ "X$input" != X ]; then # not <Enter>
+  while true; do # display promp until a valid input
+    if [[ "$input" =~ ^[0-9]+$ ]]; then # positive integer
+      if [ $input -le $max -a $input -ge $min ]; then break; fi
+    fi
+    echo; echo "Invalid run number: $input,"
+    echo "please input an integer in [$min, $max]:"
+    read -e -n ${#max} input
+  done
+  min=$input
+else
+  min=$old
+fi
+
+# specify max run number
+echo "Please set maximal run number (<Enter> to accept default: $max):"
+read -e -n ${#max} input
+if [ "X$input" != X ]; then # not <Enter>
+  while true; do # display promp until a valid input
+    if [[ "$input" =~ ^[0-9]+$ ]]; then # positive integer
+      if [ $input -le $max -a $input -ge $min ]; then break; fi
+    fi
+    echo; echo "Invalid run number: $input,"
+    echo "please input an integer in [$min, $max]:"
+    read -e -n ${#max} input
+  done
+  max=$input
+fi
+
+read -e -n1 -p "Overwrite existing root files? (y/N): " overwrite
 if [ X$overwrite = X ]; then overwrite=N; fi
-echo $overwrite
 
 # threshold for peak searching
-thr=3
+threshold=3
 echo "Please set threshold for peak searching:"
-read -p "(<Enter> to accept default: $thr ADC counts) " threshold
-if [ X$threshold = X ]; then threshold=$thr; fi
+echo "(<Enter> to accept default: $threshold ADC counts)"
+read -e -n4 input
+if [ "X$input" != X ]; then # not <Enter>
+  while true; do # display promp until a valid input
+    if [[ "$input" =~ ^[0-9]+$ ]]; then # positive integer
+      if [ $input -lt 1024 ]; then break; fi
+    fi
+    echo; echo "Invalid threshold: $input,"
+    echo "please input an integer in [1, 1024]:"
+    read -e -n4 input
+  done
+  threshold=$input
+fi
 
-echo "Loop over run [$run, $max]..."
+echo "Loop over run [$min, $max]..."
+run=$min
 while [ $run -le $max ]; do
   # specify executable
-  exe=$NICESYS/daq/CAEN/DT5751/b2r.exe
-  if [ $run -lt 107 ]; then exe=$NICESYS/daq/CAEN/V1751/b2r.exe; fi
+  if [ $run -lt 107 ]; then
+    exe=$NICESYS/daq/CAEN/V1751-106/b2r.exe
+  elif [ $run -lt 177 ]; then
+    exe=$NICESYS/daq/CAEN/DT5751-176/b2r.exe
+  else
+    exe=$NICESYS/daq/CAEN/DT5751/b2r.exe
+  fi
   # loop over sub runs
   r6d=`printf "%06d" $run`
   dir=${r6d:0:4}00
