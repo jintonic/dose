9d5462199bc33622083223cbcdec6b7010db4669
Jing LIU <jing.liu@usd.edu>
2019-01-09 23:00:15 -0600

    made it work for ROOT 6

--- macro/fit1pe.C
+++ macro/fit1pe.C
@@ -1,17 +1,37 @@
-NICE::WFs *evt = new NICE::WFs; // data structure
-const char *folder = gSystem->Getenv("NICEDAT"); // data location
-int min=99999, max=0; // integration range (will be updated automatically)
-
-// integration range is not fixed by default
-void fit1pe(int run=112, int fixedMin=0, int fixedMax=0)
+// search the range of the 1st pulse above threshold
+void SearchPulseAboveThreshold(double threshold)
 {
-   if (fixedMin!=0 && fixedMax!=0) DrawWFs(run);
-   Create1PEdistr(run, fixedMin, fixedMax);
-   Fit1PEdistr(run);
-}
+   NICE::WFs *evt = new NICE::WFs; // data structure
+   int min=99999, max=0; // integration range (will be updated automatically)
+   for (int i=0; i<evt->nmax-2; i++) {
+      if (  evt->At(0)->smpl[i]  >threshold && 
+            evt->At(0)->smpl[i+1]>threshold && 
+            evt->At(0)->smpl[i+2]>threshold) {
+         min=i;
+         break;
+      }
+   }
+   if (min==99999) min=0;
 
+   for (int i=min; i<evt->nmax; i++) {
+      if (evt->At(0)->smpl[i]<threshold) {
+         max=i;
+         break;
+      }
+   }
+   int middle = (min+max)/2;
+   min= middle-20;
+   max= middle+30;
+
+   if (min<0) min=0;
+   if (max-min<50) max=min+50;
+   if (max>evt->nmax) max=evt->nmax;
+}
 void Create1PEdistr(int run, int fixedMin, int fixedMax)
 {
+   NICE::WFs *evt = new NICE::WFs; // data structure
+   const char *folder = gSystem->Getenv("NICEDAT"); // data location
+
    TChain *t = new TChain("t");
    t->Add(Form("%s/%04d00/run_%06d.000001.root",folder,run/100,run));
 
@@ -24,6 +44,7 @@ void Create1PEdistr(int run, int fixedMin, int fixedMax)
    TH1D *hmax = new TH1D("hmax","",evt->nmax,0,evt->nmax);
    TH1D *hrange = new TH1D("hrange","",100,0,100);
 
+   int min=99999, max=0; // integration range (will be updated automatically)
    int nevt = t->GetEntries();
    if (nevt>50000) nevt=50000; // no need to load more than this
    for(int i=0; i<nevt; i++) {
@@ -74,12 +95,12 @@ void Fit1PEdistr(int run)
          " + [7]*exp(-0.5*((x-3*[4])/[5])**2)", -50,200);
    f->SetParNames("n0", "m0", "s0", "norm", "mean", "sigma", "n2", "n3");
    f->SetParameter(1,0);
-   f->SetParameter(2,4);
-   f->SetParameter(4,30);
-   f->SetParameter(5,10);
+   f->SetParameter(2,7.6);
+   f->SetParameter(4,42);
+   f->SetParameter(5,16);
    f->SetParLimits(5, 6, 26);
-   f->SetParLimits(6, 0, 500);
-   f->SetParLimits(7, 0, 500);
+   //f->SetParLimits(6, 0, 500);
+   //f->SetParLimits(7, 0, 500);
    hpe->Fit(f);
 
    TF1 *baseline = new TF1("baseline","gaus",-50,50);
@@ -115,6 +136,7 @@ void Fit1PEdistr(int run)
 void DrawWFs(int run)
 {
    TChain *t = new TChain("t");
+   const char *folder = gSystem->Getenv("NICEDAT"); // data location
    t->Add(Form("%s/%04d00/run_%06d.000001.root",folder,run/100,run));
    t->Draw("wf[0].smpl:Iteration$","abs(wf[0].npe)<100","l",250,1);
    TText *text = new TText(.8,.8,Form("%d",run));
@@ -123,31 +145,12 @@ void DrawWFs(int run)
    gPad->Print(Form("wf%d.png",run));
 }
 
-// search the range of the 1st pulse above threshold
-void SearchPulseAboveThreshold(double threshold)
+
+// integration range is not fixed by default
+void fit1pe(int run=112, int fixedMin=0, int fixedMax=0)
 {
-   for (int i=0; i<evt->nmax-2; i++) {
-      if (  evt->At(0)->smpl[i]  >threshold && 
-            evt->At(0)->smpl[i+1]>threshold && 
-            evt->At(0)->smpl[i+2]>threshold) {
-         min=i;
-         break;
-      }
-   }
-   if (min==99999) min=0;
-
-   for (int i=min; i<evt->nmax; i++) {
-      if (evt->At(0)->smpl[i]<threshold) {
-         max=i;
-         break;
-      }
-   }
-   int middle = (min+max)/2;
-   min= middle-20;
-   max= middle+30;
-
-   if (min<0) min=0;
-   if (max-min<50) max=min+50;
-   if (max>evt->nmax) max=evt->nmax;
+   if (fixedMin!=0 && fixedMax!=0) DrawWFs(run);
+   Create1PEdistr(run, fixedMin, fixedMax);
+   Fit1PEdistr(run);
 }
 
